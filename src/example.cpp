05c244d6c04eb7485dd23955f1e206c5961cb5c3
Jacques KAISER <jackokaiser@gmail.com>
2014-12-04 18:25:17 +0100

    removing obstacles from images

--- src/example.cpp
+++ src/example.cpp
@@ -4,6 +4,13 @@
 #include <vector>
 #include <string>
 
+#define INTRINSIC_U0 258.
+#define INTRINSIC_V0 156.
+#define INTRINSIC_ALPHA_U 410.
+#define INTRINSIC_ALPHAU_V 410.
+#define STEREO_BASELINE 0.22
+#define CAMERA_HEIGHT 1.128
+
 using namespace cv;
 using namespace std;
 
@@ -18,9 +25,30 @@ void loadStereoImg (vector<Mat>& leftImages, vector<Mat>& rightImages) {
   }
 }
 
-void initializeDisparityMaps (vector<Mat>& disparityMap, const Size& size, unsigned int numberOfImages) {
+void initializeDisparityMaps (vector<Mat>& disparityMaps, const Size& size, unsigned int numberOfImages) {
   for (unsigned int i=0; i < numberOfImages; i++) {
-    disparityMap.push_back(Mat(size, CV_16UC1));
+    disparityMaps.push_back(Mat(size, CV_16UC1));
+  }
+}
+
+void filterOutObstacles (Mat& disparityMap, float tooCloseThreshold=0.2, float tooHighThreshold=2.5) {
+  Point3f pixelInWorld;
+  float* linePointer;
+  float disparityMapValue;
+
+  for(int i = 0; i < disparityMap.rows; i++) {
+    linePointer = disparityMap.ptr<float>(i);
+    for (int j = 0; j < disparityMap.cols; j++) {
+      disparityMapValue = linePointer[j];
+
+      pixelInWorld.x = (i - INTRINSIC_U0) * STEREO_BASELINE / disparityMapValue - STEREO_BASELINE / 2.;
+      pixelInWorld.y = INTRINSIC_ALPHA_U * STEREO_BASELINE / disparityMapValue;
+      pixelInWorld.z = CAMERA_HEIGHT - (j - INTRINSIC_V0) * INTRINSIC_ALPHA_U * STEREO_BASELINE / (INTRINSIC_ALPHAU_V * disparityMapValue);
+
+      if ((pixelInWorld.z < tooCloseThreshold) || (pixelInWorld.y > tooHighThreshold)) {
+        linePointer[j] = 0.;
+      }
+    }
   }
 }
 
@@ -35,14 +63,19 @@ int main(int argc, char **argv)
   unsigned int numberOfImages = leftImages.size();
 
   // initialize the disparityMap with empty images of the right size
-  vector<Mat> disparityMap;
-  initializeDisparityMaps(disparityMap, leftImages[0].size(), numberOfImages);
+  vector<Mat> disparityMaps;
+  initializeDisparityMaps(disparityMaps, leftImages[0].size(), numberOfImages);
 
   // compute disparityMaps
   StereoSGBM sgbm = StereoSGBM(0, 32, 7, 8*7*7, 32*7*7, 2, 0, 5, 100, 32, true);
   for (unsigned int i=0; i < numberOfImages; i++) {
-    sgbm( leftImages[i], rightImages[i], disparityMap[i]);
-    disparityMap[i].convertTo(displayImg, CV_8UC1);
+    sgbm( leftImages[i], rightImages[i], disparityMaps[i]);
+
+    disparityMaps[i].convertTo(disparityMaps[i], CV_32F);
+    filterOutObstacles(disparityMaps[i]);
+
+
+    disparityMaps[i].convertTo(displayImg, CV_8UC1);
     imshow("disparity map", displayImg);
     waitKey();
   };
